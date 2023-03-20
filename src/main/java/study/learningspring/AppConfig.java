7ead5e877fa625280ee426520390f83537510748
1417mhz <Dsarkk3345@>
2023-03-20 12:25:14 +0900

    스프링 도입

--- src/main/java/study/learningspring/AppConfig.java
+++ src/main/java/study/learningspring/AppConfig.java
@@ -1,7 +1,10 @@
 package study.learningspring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import study.learningspring.discount.DiscountPolicy;
 import study.learningspring.discount.FixDiscountPolicy;
+import study.learningspring.discount.RateDiscountPolicy;
 import study.learningspring.member.MemberRepository;
 import study.learningspring.member.MemberService;
 import study.learningspring.member.MemberServiceImpl;
@@ -9,20 +12,26 @@ import study.learningspring.member.MemoryMemberRepository;
 import study.learningspring.order.OrderService;
 import study.learningspring.order.OrderServiceImpl;
 
+// 스프링에서 관리하는 설정 정보임을 뜻하는 어노테이션
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
--- src/main/java/study/learningspring/MemberApp.java
+++ src/main/java/study/learningspring/MemberApp.java
@@ -1,5 +1,7 @@
 package study.learningspring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.learningspring.member.Grade;
 import study.learningspring.member.Member;
 import study.learningspring.member.MemberService;
@@ -7,10 +9,14 @@ import study.learningspring.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 
-//        MemberService memberService = new MemberServiceImpl();
+        // AppConfig에 있는 설정 정보를 가져다 사용할 수 있도록 해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // .getBean(빈 이름, 타입.class)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
