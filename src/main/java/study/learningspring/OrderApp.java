8ab80b881cdaf1bd43bdc93f7f1c084163f25454
ysw <ysw99@kakao.com>
2023-03-20 12:25:14 +0900

    스프링 도입

--- src/main/java/study/learningspring/OrderApp.java
+++ src/main/java/study/learningspring/OrderApp.java
@@ -1,5 +1,7 @@
 package study.learningspring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.learningspring.member.Grade;
 import study.learningspring.member.Member;
 import study.learningspring.member.MemberService;
@@ -10,15 +12,19 @@ import study.learningspring.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
     }
