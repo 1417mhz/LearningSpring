8f31e992c6ab3a9b98b66469cfb5c584fc1bd146
ysw <ysw99@kakao.com>
2023-04-06 00:34:26 +0900

    다형성을 이용한 빈 동적 활용 - 보충설명 추가

--- src/test/java/study/learningspring/autowired/AllBeanTest.java
+++ src/test/java/study/learningspring/autowired/AllBeanTest.java
@@ -19,28 +19,38 @@ public class AllBeanTest {
 
     @Test
     void findAllBean() {
+        // AnnotationConfigApplicationContext 를 통해 스프링 컨테이너를 생성하고
+        // AutoAppConfig 와 DiscountService 는 컴포넌트 스캔을 통해 빈을 등록한다
+        // DiscountPolicy 타입 빈을 가져오기 위해 AutoAppConfig 도 등록한다
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
-
         DiscountService discountService = ac.getBean(DiscountService.class);
+
         Member member = new Member(1L, "userA", Grade.VIP);
+
+        // 어떤 할인 정책을 이용할지를 인자로 메소드에 전달한다
         int fixDiscountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
 
         assertThat(discountService).isInstanceOf(DiscountService.class);
         assertThat(fixDiscountPrice).isEqualTo(1000);
 
+        // 어떤 할인 정책을 이용할지를 인자로 메소드에 전달한다
         int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
         assertThat(rateDiscountPrice).isEqualTo(2000);
     }
 
+    // 기존 코드를 건드리면 복잡해지므로 별도로 생성한 서비스 코드
     static class DiscountService {
-        // DiscountPolicy 타입으로 등록되어있는 빈들이 Map, List 타입으로 담긴다
+
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policyList;
 
-//        @Autowired
+        @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
+            // DiscountPolicy 타입으로 등록되어있는 빈들을 가져와 Map, List 타입으로 담는다
             this.policyMap = policyMap;
             this.policyList = policyList;
+
+            /* 의존관계 주입 여부 확인 **/
             // Map 은 키:값 형태로 빈 이름:빈 인스턴스 형태로 출력됨
             System.out.println("policyMap = " + policyMap);
             // List 는 스프링 빈 인스턴스가 출력됨
@@ -49,12 +59,16 @@ public class AllBeanTest {
 
         // 다형성을 이용한 유연함 발휘
         public int discount(Member member, int price, String discountCode) {
-            // 인자로 넘어온 discount 정책 이름에 따라 Map 에서 해당 빈을 가져와 로직을 실행시킨다
+            /* 인자로 넘어온 할인 정책 이름에 따라 Map 에서 해당 빈을 가져와 로직을 실행시킨다
+               할인 정책 이름(코드)을 빈 이름과 일치시킨다 **/
             DiscountPolicy discountPolicy = policyMap.get(discountCode);
 
+            // 어떤 할인 정책으로 동작하는지 확인하기 위한 코드이다. 실제 사용자는 인터페이스만 알 뿐 어떤 구현체인지는 알 수 없다.
+            // Code = 할인 정책의 이름, Policy = 실제 할인 정책 빈 인스턴스
             System.out.println("discountCode = " + discountCode);
             System.out.println("discountPolicy = " + discountPolicy);
 
+            // return 값은 int
             return discountPolicy.discount(member, price);
         }
     }
